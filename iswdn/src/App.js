11afbd92dc3cea45b468e967b615a8154eea1cbf
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-10-04 00:06:00 -0400

    first

---
+++ iswdn/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/navBar/navBar';
+import Home from './Components/Home/home';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
